b7a50a14b0dc2a3329d71001a66a4b088521d26c
shkimgit <shkim4936@outlook.com>
2016-05-25 15:25:37 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 // For command line git tutorial
 // First : Local repository modification
-
 // Second : GitHub modification
->>>>>>> origin/master
+
 print("hello world")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
